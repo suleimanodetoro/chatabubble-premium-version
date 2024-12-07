778a915f3e2055142fec6fc99de9268bbef8c27a
Suleiman Odetoro <odetoro75@gmail.com>
2024-12-07 16:35:28 +0000

    Add new env files for supabase config

--- app.config.js
+++ app.config.js
@@ -1,11 +1,13 @@
 // app.config.js
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
-    expo: {
-      // ... other config
-      extra: {
-        OPENAI_API_KEY: process.env.EXPO_PUBLIC_OPENAI_API_KEY,
-      },
+  expo: {
+    // ... other config
+    extra: {
+      OPENAI_API_KEY: process.env.EXPO_PUBLIC_OPENAI_API_KEY,
+      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
     },
-  };
+  },
+};
